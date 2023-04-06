2962067ae574ad8920ef3a83f73b8d17ad5c5d7d
310-1 <310-1@310-21>
2023-04-06 12:49:05 +0900

    2023

---
+++ src/main/java/com/sg/leo/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.sg.leo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/html")
+	public String html() {
+		System.out.println("HTML파일이 요청됨");
+		return "redirect:starthere.html";
+	}
+	
+	@GetMapping("/img")
+	public String img() {
+		System.out.println("이미지파일이 요청됨");
+		return "redirect:chatjpt.png";
+	}
+	
+	@GetMapping("/jsp")
+	public String jsp(Model model) {
+		System.out.println("JSP 파일이 요청됨 hellocontroller");
+		model.addAttribute("username", "흐흠 이 콘텐츠주인: 이태희");
+		return "hellojsp";           
+	}
+}
